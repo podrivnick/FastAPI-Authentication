3564bd4299b1ac74a27776798dce8fc34750cb59
podrivnick <ribakov1973@gmail.com>
2024-09-03 15:46:12 +0300

    Test for Password value object.

--- src/tests/domain/value_object/test_user_data.py
+++ src/tests/domain/value_object/test_user_data.py
@@ -1,5 +1,10 @@
 import pytest
 from src.domain.user import value_objects as vo
+from src.domain.user.value_objects.password import (
+    EmptyPasswordError,
+    TooLongPasswordError,
+    WrongPasswordFormatError,
+)
 from src.domain.user.value_objects.username import (
     EmptyUsernameError,
     TooLongUsernameError,
@@ -20,3 +25,18 @@ def test_username_is_valid():
 
     with pytest.raises(WrongUsernameFormatError):
         vo.UserName(username_wrong__format)
+
+
+def test_password_is_valid():
+    password_empty = ""
+    password_is_too_long = "Passweodsffveedffffffffffffffffffffffffffffffffffxdfdf2"
+    password_wrong_format = "3445343434"
+
+    with pytest.raises(EmptyPasswordError):
+        vo.Password(password_empty)
+
+    with pytest.raises(TooLongPasswordError):
+        vo.Password(password_is_too_long)
+
+    with pytest.raises(WrongPasswordFormatError):
+        vo.Password(password_wrong_format)
